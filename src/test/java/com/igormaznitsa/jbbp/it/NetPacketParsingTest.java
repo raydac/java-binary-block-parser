b3a5ba135550e5dc7c34899ab57ab46024d6c180
Igor Maznitsa <rrg4400@gmail.com>
2016-04-18 14:11:09 +0300

    improved NetPacketParsingTest#testParsingTCPFrameInsideNetworkFrame(), added extraction of Ethernet and IP headers

--- src/test/java/com/igormaznitsa/jbbp/it/NetPacketParsingTest.java
+++ src/test/java/com/igormaznitsa/jbbp/it/NetPacketParsingTest.java
@@ -22,118 +22,195 @@ import com.igormaznitsa.jbbp.mapper.Bin;
 import com.igormaznitsa.jbbp.mapper.BinType;
 import com.igormaznitsa.jbbp.model.*;
 import com.igormaznitsa.jbbp.utils.JBBPUtils;
-import java.io.InputStream;
 import static org.junit.Assert.*;
+import java.io.StringWriter;
 import org.junit.Test;
+import com.igormaznitsa.jbbp.io.JBBPBitInputStream;
+import com.igormaznitsa.jbbp.utils.JBBPTextWriter;
 
 public class NetPacketParsingTest extends AbstractParserIntegrationTest {
 
   @Test
   public void testParsingTCPFrameInsideNetworkFrame() throws Exception {
-    final InputStream tcpFrameStream = getResourceAsInputStream("tcppacket.bin");
+    final JBBPBitInputStream netPacketStream = new JBBPBitInputStream(getResourceAsInputStream("tcppacket.bin"));
     try {
 
-      final JBBPParser tcpParser = JBBPParser.prepare(
-              "skip:34; // skip bytes till the frame\n"
-              + "ushort SourcePort;"
-              + "ushort DestinationPort;"
-              + "int SequenceNumber;"
-              + "int AcknowledgementNumber;"
-              + "bit:1 NONCE;"
-              + "bit:3 RESERVED;"
-              + "bit:4 HLEN;"
-              + "bit:1 FIN;"
-              + "bit:1 SYN;"
-              + "bit:1 RST;"
-              + "bit:1 PSH;"
-              + "bit:1 ACK;"
-              + "bit:1 URG;"
-              + "bit:1 ECNECHO;"
-              + "bit:1 CWR;"
-              + "ushort WindowSize;"
-              + "ushort TCPCheckSum;"
-              + "ushort UrgentPointer;"
-              + "byte [$$-34-HLEN*4] Option;"
-              + "byte [_] Data;"
+      // Minimal ethernet header without 802.1Q tag
+      final JBBPParser ethernetParserHeaderWithout802_1QTag = JBBPParser.prepare(
+        "byte[6] MacDestination;"
+        +"byte[6] MacSource;"
+        +"ushort EthertypeOrLength;" 
+      );
+      
+      // Minimal IP header without options
+      final JBBPParser ipParserHeaderWithoutOptions = JBBPParser.prepare(
+          "bit:4 InternetHeaderLength;"
+          +"bit:4 Version;"
+          +"bit:2 ECN;"
+          +"bit:6 DSCP;"
+          +"ushort TotalPacketLength;"
+          +"ushort Identification;"
+          +"ushort IPFlagsAndFragmentOffset;"
+          +"ubyte TTL;"
+          +"ubyte Protocol;"
+          +"ushort HeaderChecksum;"
+          +"int SourceAddress;"
+          +"int DestinationAddress;"
       );
 
-      final JBBPFieldStruct result = tcpParser.parse(tcpFrameStream);
+      final JBBPParser tcpHeader = JBBPParser.prepare(
+          "ushort SourcePort;"
+          + "ushort DestinationPort;"
+          + "int SequenceNumber;"
+          + "int AcknowledgementNumber;"
+          + "bit:1 NONCE;"
+          + "bit:3 RESERVED;"
+          + "bit:4 HLEN;"
+          + "bit:1 FIN;"
+          + "bit:1 SYN;"
+          + "bit:1 RST;"
+          + "bit:1 PSH;"
+          + "bit:1 ACK;"
+          + "bit:1 URG;"
+          + "bit:1 ECNECHO;"
+          + "bit:1 CWR;"
+          + "ushort WindowSize;"
+          + "ushort TCPCheckSum;"
+          + "ushort UrgentPointer;"
+          + "byte [$$-HLEN*4] Option;"
+      );
 
-      assertEquals(40018, result.findFieldForNameAndType("SourcePort", JBBPFieldUShort.class).getAsInt());
-      assertEquals(56344, result.findFieldForNameAndType("DestinationPort", JBBPFieldUShort.class).getAsInt());
-      assertEquals(0xE0084171, result.findFieldForNameAndType("SequenceNumber", JBBPFieldInt.class).getAsInt());
-      assertEquals(0xAB616F71, result.findFieldForNameAndType("AcknowledgementNumber", JBBPFieldInt.class).getAsInt());
+      
+      // Check Ethernet header
+      final JBBPFieldStruct parsedEthernetHeader = ethernetParserHeaderWithout802_1QTag.parse(netPacketStream);
+      assertArrayEquals(new byte[]{(byte)0x60, (byte)0x67, (byte)0x20, (byte)0xE1, (byte)0xF9, (byte)0xF8},parsedEthernetHeader.findFieldForNameAndType("MacDestination", JBBPFieldArrayByte.class).getArray());
+      assertArrayEquals(new byte[]{(byte)0x00, (byte)0x26, (byte)0x44, (byte)0x74, (byte)0xFE, (byte)0x66},parsedEthernetHeader.findFieldForNameAndType("MacSource", JBBPFieldArrayByte.class).getArray());
+      assertEquals(0x800, parsedEthernetHeader.findFieldForNameAndType("EthertypeOrLength", JBBPFieldUShort.class).getAsInt());
+      
+      // Check IP header
+      netPacketStream.resetCounter();
+      final JBBPFieldStruct parsedIPHeader = ipParserHeaderWithoutOptions.parse(netPacketStream);
 
-      assertFalse(result.findFieldForNameAndType("FIN", JBBPFieldBit.class).getAsBool());
-      assertFalse(result.findFieldForNameAndType("SYN", JBBPFieldBit.class).getAsBool());
-      assertFalse(result.findFieldForNameAndType("RST", JBBPFieldBit.class).getAsBool());
-      assertTrue(result.findFieldForNameAndType("PSH", JBBPFieldBit.class).getAsBool());
-      assertTrue(result.findFieldForNameAndType("ACK", JBBPFieldBit.class).getAsBool());
-      assertFalse(result.findFieldForNameAndType("URG", JBBPFieldBit.class).getAsBool());
-      assertFalse(result.findFieldForNameAndType("ECNECHO", JBBPFieldBit.class).getAsBool());
-      assertFalse(result.findFieldForNameAndType("CWR", JBBPFieldBit.class).getAsBool());
-      assertFalse(result.findFieldForNameAndType("NONCE", JBBPFieldBit.class).getAsBool());
-      assertFalse(result.findFieldForNameAndType("RESERVED", JBBPFieldBit.class).getAsBool());
+      assertEquals(4, parsedIPHeader.findFieldForNameAndType("Version",JBBPFieldBit.class).getAsInt());
+      assertEquals(5, parsedIPHeader.findFieldForNameAndType("InternetHeaderLength",JBBPFieldBit.class).getAsInt());
+      assertEquals(0, parsedIPHeader.findFieldForNameAndType("DSCP",JBBPFieldBit.class).getAsInt());
+      assertEquals(0, parsedIPHeader.findFieldForNameAndType("ECN",JBBPFieldBit.class).getAsInt());
+      
+      final int ipTotalPacketLength = parsedIPHeader.findFieldForNameAndType("TotalPacketLength", JBBPFieldUShort.class).getAsInt();
+      
+      assertEquals(159, ipTotalPacketLength);
+      assertEquals(30810, parsedIPHeader.findFieldForNameAndType("Identification",JBBPFieldUShort.class).getAsInt());
+      
+      final int ipFlagsAndFragmentOffset = parsedIPHeader.findFieldForNameAndType("IPFlagsAndFragmentOffset", JBBPFieldUShort.class).getAsInt();
+      
+      assertEquals("Extracted IP flags", 0x2, ipFlagsAndFragmentOffset>>>13);
+      assertEquals("Extracted Fragment offset", 0x00, ipFlagsAndFragmentOffset & 0x1FFF);
+      
+      assertEquals(0x39, parsedIPHeader.findFieldForNameAndType("TTL",JBBPFieldUByte.class).getAsInt());
+      assertEquals(0x06, parsedIPHeader.findFieldForNameAndType("Protocol",JBBPFieldUByte.class).getAsInt());
+      assertEquals(0x7DB6, parsedIPHeader.findFieldForNameAndType("HeaderChecksum",JBBPFieldUShort.class).getAsInt());
+      assertEquals(0xD5C7B393, parsedIPHeader.findFieldForNameAndType("SourceAddress",JBBPFieldInt.class).getAsInt());
+      assertEquals(0xC0A80145, parsedIPHeader.findFieldForNameAndType("DestinationAddress",JBBPFieldInt.class).getAsInt());
+      
+      // Check TCP header
+      netPacketStream.resetCounter();
+      final JBBPFieldStruct parsedTcpHeader = tcpHeader.parse(netPacketStream);
 
-      assertEquals(5, result.findFieldForNameAndType("HLEN", JBBPFieldBit.class).getAsInt());
+      assertEquals(40018, parsedTcpHeader.findFieldForNameAndType("SourcePort", JBBPFieldUShort.class).getAsInt());
+      assertEquals(56344, parsedTcpHeader.findFieldForNameAndType("DestinationPort", JBBPFieldUShort.class).getAsInt());
+      assertEquals(0xE0084171, parsedTcpHeader.findFieldForNameAndType("SequenceNumber", JBBPFieldInt.class).getAsInt());
+      assertEquals(0xAB616F71, parsedTcpHeader.findFieldForNameAndType("AcknowledgementNumber", JBBPFieldInt.class).getAsInt());
 
-      assertEquals(40880, result.findFieldForNameAndType("WindowSize", JBBPFieldUShort.class).getAsInt());
-      assertEquals(0x8BB6, result.findFieldForNameAndType("TCPCheckSum", JBBPFieldUShort.class).getAsInt());
-      assertEquals(0, result.findFieldForNameAndType("UrgentPointer", JBBPFieldUShort.class).getAsInt());
+      assertFalse(parsedTcpHeader.findFieldForNameAndType("FIN", JBBPFieldBit.class).getAsBool());
+      assertFalse(parsedTcpHeader.findFieldForNameAndType("SYN", JBBPFieldBit.class).getAsBool());
+      assertFalse(parsedTcpHeader.findFieldForNameAndType("RST", JBBPFieldBit.class).getAsBool());
+      assertTrue(parsedTcpHeader.findFieldForNameAndType("PSH", JBBPFieldBit.class).getAsBool());
+      assertTrue(parsedTcpHeader.findFieldForNameAndType("ACK", JBBPFieldBit.class).getAsBool());
+      assertFalse(parsedTcpHeader.findFieldForNameAndType("URG", JBBPFieldBit.class).getAsBool());
+      assertFalse(parsedTcpHeader.findFieldForNameAndType("ECNECHO", JBBPFieldBit.class).getAsBool());
+      assertFalse(parsedTcpHeader.findFieldForNameAndType("CWR", JBBPFieldBit.class).getAsBool());
+      assertFalse(parsedTcpHeader.findFieldForNameAndType("NONCE", JBBPFieldBit.class).getAsBool());
+      assertFalse(parsedTcpHeader.findFieldForNameAndType("RESERVED", JBBPFieldBit.class).getAsBool());
 
-      assertEquals(0, result.findFieldForNameAndType("Option", JBBPFieldArrayByte.class).size());
-      assertEquals(119, result.findFieldForNameAndType("Data", JBBPFieldArrayByte.class).size());
+      assertEquals(5, parsedTcpHeader.findFieldForNameAndType("HLEN", JBBPFieldBit.class).getAsInt());
 
-    }
-    finally {
-      JBBPUtils.closeQuietly(tcpFrameStream);
+      assertEquals(40880, parsedTcpHeader.findFieldForNameAndType("WindowSize", JBBPFieldUShort.class).getAsInt());
+      assertEquals(0x8BB6, parsedTcpHeader.findFieldForNameAndType("TCPCheckSum", JBBPFieldUShort.class).getAsInt());
+      assertEquals(0, parsedTcpHeader.findFieldForNameAndType("UrgentPointer", JBBPFieldUShort.class).getAsInt());
+
+      assertEquals(0, parsedTcpHeader.findFieldForNameAndType("Option", JBBPFieldArrayByte.class).size());
+      
+      // extract data
+      final int payloadDataLength = ipTotalPacketLength - 20 - (int)netPacketStream.getCounter();
+      final byte [] data = netPacketStream.readByteArray(payloadDataLength);
+      assertEquals(119, data.length);
+      
+      System.out.println(new JBBPTextWriter(new StringWriter()).Comment("Payload data of the Net packet").Byte(data).BR().toString());
+      
+      final byte [] rest = netPacketStream.readByteArray(-1);
+      assertEquals(0,rest.length);
+      
+    } finally {
+      JBBPUtils.closeQuietly(netPacketStream);
     }
   }
 
   @Test
   public void testParseSomePacketGettedOverTCP_ExampleFromStackOverflow() throws Exception {
     final class Parsed {
-      @Bin(outOrder = 1) byte begin;
-      @Bin(outOrder = 2, type = BinType.BIT, outBitNumber = JBBPBitNumber.BITS_4) int version;
-      @Bin(outOrder = 3, type = BinType.BIT, outBitNumber = JBBPBitNumber.BITS_4) int returnType;
-      @Bin(outOrder = 4) byte[] productCode;
-      @Bin(outOrder = 5, type = BinType.USHORT) int dataLength;
+
+      @Bin(outOrder = 1)
+      byte begin;
+      @Bin(outOrder = 2, type = BinType.BIT, outBitNumber = JBBPBitNumber.BITS_4)
+      int version;
+      @Bin(outOrder = 3, type = BinType.BIT, outBitNumber = JBBPBitNumber.BITS_4)
+      int returnType;
+      @Bin(outOrder = 4)
+      byte[] productCode;
+      @Bin(outOrder = 5, type = BinType.USHORT)
+      int dataLength;
     }
-    
-    final byte [] testArray =  new byte[]{0x23, 0x21, (byte) 0x90, 0x23, 0x21, 0x22, 0x12, 0x00, (byte) 0xAA};
-    
+
+    final byte[] testArray = new byte[]{0x23, 0x21, (byte) 0x90, 0x23, 0x21, 0x22, 0x12, 0x00, (byte) 0xAA};
+
     final Parsed parsed = JBBPParser.prepare("byte begin; bit:4 version; bit:4 returnType; byte [5] productCode; ushort dataLength;")
-            .parse(testArray)
-            .mapTo(Parsed.class);
+        .parse(testArray)
+        .mapTo(Parsed.class);
 
     assertEquals(0x23, parsed.begin);
     assertEquals(0x01, parsed.version);
     assertEquals(0x02, parsed.returnType);
     assertArrayEquals(new byte[]{(byte) 0x90, 0x23, 0x21, 0x22, 0x12}, parsed.productCode);
     assertEquals(0x00AA, parsed.dataLength);
-    
+
     assertArrayEquals(testArray, JBBPOut.BeginBin().Bin(parsed).End().toByteArray());
   }
 
   @Test
   public void testParseUDP() throws Exception {
     final class Parsed {
-      @Bin(outOrder = 1) char source;
-      @Bin(outOrder = 2) char destination;
-      @Bin(outOrder = 3) char length;
-      @Bin(outOrder = 4) char checksum;
-      @Bin(outOrder = 5) byte[] data;
+
+      @Bin(outOrder = 1)
+      char source;
+      @Bin(outOrder = 2)
+      char destination;
+      @Bin(outOrder = 3)
+      char length;
+      @Bin(outOrder = 4)
+      char checksum;
+      @Bin(outOrder = 5)
+      byte[] data;
     }
 
-    final byte [] testArray = new byte[]{0x04, (byte) 0x89, 0x00, 0x35, 0x00, 0x2C, (byte) 0xAB, (byte) 0xB4, 0x00, 0x01, 0x01, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x04, 0x70, 0x6F, 0x70, 0x64, 0x02, 0x69, 0x78, 0x06, 0x6E, 0x65, 0x74, 0x63, 0x6F, 0x6D, 0x03, 0x63, 0x6F, 0x6D, 0x00, 0x00, 0x01, 0x00, 0x01};
-    
+    final byte[] testArray = new byte[]{0x04, (byte) 0x89, 0x00, 0x35, 0x00, 0x2C, (byte) 0xAB, (byte) 0xB4, 0x00, 0x01, 0x01, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x04, 0x70, 0x6F, 0x70, 0x64, 0x02, 0x69, 0x78, 0x06, 0x6E, 0x65, 0x74, 0x63, 0x6F, 0x6D, 0x03, 0x63, 0x6F, 0x6D, 0x00, 0x00, 0x01, 0x00, 0x01};
+
     final Parsed parsed = JBBPParser.prepare("ushort source; ushort destination; ushort length; ushort checksum; byte [length-8] data;").parse(testArray).mapTo(Parsed.class);
-    
+
     assertEquals(0x0489, parsed.source);
     assertEquals(0x0035, parsed.destination);
     assertEquals(0xABB4, parsed.checksum);
-    assertEquals(0x002C-8, parsed.data.length);
-    
+    assertEquals(0x002C - 8, parsed.data.length);
+
     assertArrayEquals(testArray, JBBPOut.BeginBin().Bin(parsed).End().toByteArray());
   }
 
