c03cb1c56a1d08c621d1c459fd551a7e099b6b1f
Igor Maznitsa <rrg4400@gmail.com>
2017-08-01 20:21:14 +0300

    added method to check png chunk

--- src/test/java/com/igormaznitsa/jbbp/TestUtils.java
+++ src/test/java/com/igormaznitsa/jbbp/TestUtils.java
@@ -13,17 +13,18 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.igormaznitsa.jbbp;
 
+import static org.junit.Assert.assertEquals;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
+import java.util.zip.CRC32;
 
 /**
  * Different useful auxiliary test methods
  */
 public enum TestUtils {
-    ;
+  ;
 
     /**
      * Inject new value into final field
@@ -35,31 +36,68 @@ public enum TestUtils {
      * @throws Exception it will be thrown for any error
      */
     public static void injectDeclaredFinalFieldValue(final Class<?> klazz, final Object instance, final String fieldName, final Object value) throws Exception {
-        final Field field = klazz.getDeclaredField(fieldName);
-        field.setAccessible(true);
+    final Field field = klazz.getDeclaredField(fieldName);
+    field.setAccessible(true);
 
-        final Field modifiersField = Field.class.getDeclaredField("modifiers");
-        modifiersField.setAccessible(true);
-        modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
+    final Field modifiersField = Field.class.getDeclaredField("modifiers");
+    modifiersField.setAccessible(true);
+    modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
 
-        field.set(instance, value);
-    }
+    field.set(instance, value);
+  }
 
-    /**
-     * Read field value, also allows to provide dot-separated chain of fields
-     * @param <T> expected type of value
-     * @param instance instance of object, must not be null
-     * @param fieldName field name, can be single name or dot-separated one, must not be null 
-     * @param klazz expected value class, must not be null
-     * @return value, can be null
-     * @throws Exception it will be thrown if any error
-     */
+  /**
+   * Read field value, also allows to provide dot-separated chain of fields
+   *
+   * @param <T> expected type of value
+   * @param instance instance of object, must not be null
+   * @param fieldName field name, can be single name or dot-separated one, must
+   * not be null
+   * @param klazz expected value class, must not be null
+   * @return value, can be null
+   * @throws Exception it will be thrown if any error
+   */
   public static <T> T getField(final Object instance, final String fieldName, final Class<T> klazz) throws Exception {
-    final String [] fields = fieldName.split("\\.");
+    final String[] fields = fieldName.split("\\.");
     Object result = instance;
     for (final String f : fields) {
       result = result.getClass().getField(f).get(result);
     }
     return klazz.cast(result);
   }
+
+  /**
+   * Check PNG chunk data.
+   * @param etalonName etalon type name, must not be null
+   * @param etalonLength etalon length in bytes
+   * @param chunkType chunk type
+   * @param chunkLength chunk data length
+   * @param chunkCrc chunk crc field value
+   * @param chunkData chunk data, must not be null
+   */
+  public static void assertPngChunk(final String etalonName, final int etalonLength, final int chunkType, final int chunkLength, final int chunkCrc, final byte[] chunkData) {
+    final int chunkEtalonName = (etalonName.charAt(0) << 24) | (etalonName.charAt(1) << 16) | (etalonName.charAt(2) << 8) | etalonName.charAt(3);
+
+    assertEquals("Chunk must be " + etalonName, chunkEtalonName, chunkType);
+    assertEquals("Chunk length must be " + etalonLength, etalonLength, chunkLength);
+
+    final CRC32 crc32 = new CRC32();
+    crc32.update(etalonName.charAt(0));
+    crc32.update(etalonName.charAt(1));
+    crc32.update(etalonName.charAt(2));
+    crc32.update(etalonName.charAt(3));
+
+    if (etalonLength != 0) {
+      assertEquals("Data array " + etalonName + " must be " + etalonLength, etalonLength, chunkData.length);
+      crc32.update(chunkData);
+    }
+
+    final int crc = (int) crc32.getValue();
+    assertEquals("CRC32 for " + etalonName + " must be " + crc, crc, chunkCrc);
+  }
+
+  public static String wavInt2Str(final int value) {
+    return new String(new char[]{(char) (value & 0xFF), (char) ((value >>> 8) & 0xFF), (char) ((value >>> 16) & 0xFF), (char) (value >>> 24)});
+  }
+
 }
