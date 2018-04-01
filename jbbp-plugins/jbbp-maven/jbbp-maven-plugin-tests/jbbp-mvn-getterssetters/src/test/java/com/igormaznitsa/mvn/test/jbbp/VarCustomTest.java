df6f78617695cb74dbdfc8d7d22c2a72c6021027
Igor Maznitsa <rrg4400@gmail.com>
2018-04-01 21:19:31 +0300

    fixed missed import

--- jbbp-plugins/jbbp-maven/jbbp-maven-plugin-tests/jbbp-mvn-getterssetters/src/test/java/com/igormaznitsa/mvn/test/jbbp/VarCustomTest.java
+++ jbbp-plugins/jbbp-maven/jbbp-maven-plugin-tests/jbbp-mvn-getterssetters/src/test/java/com/igormaznitsa/mvn/test/jbbp/VarCustomTest.java
@@ -26,6 +26,7 @@ import java.io.ByteArrayOutputStream;
 import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class VarCustomTest {
